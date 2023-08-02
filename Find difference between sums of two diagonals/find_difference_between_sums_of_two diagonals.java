4a3bc37699ccbc86621f3ac219b40f230f48f400
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2023-08-02 19:28:07 +0530

    Create find_difference_between_sums_of_two diagonals.java

---
+++ Find difference between sums of two diagonals/find_difference_between_sums_of_two diagonals.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class find_difference_between_sums_of_two_diagonals
+{
+    
+    public static int calculate(int a[][]){
+        int ca=0,c=0;
+		for(int i=0;i<a.length;i++){
+		    for(int j=0;j<a[0].length;j++){
+		        if(i==j)
+		            c+=a[i][j];
+		        if(i==a[0].length-j-1)
+		            ca+=a[i][j];
+		    }
+		}
+		int d=Math.abs(c-ca);
+		return d;
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int row = sc.nextInt();
+	    int col = sc.nextInt();
+		int a[][]= new int[row][col];
+		for(int i=0;i<row;i++){
+		    for(int j=0;j<col;j++){
+		        a[i][j]=sc.nextInt();
+		    }
+		}
+		
+		System.out.println(calculate(a));
+	}
+}
